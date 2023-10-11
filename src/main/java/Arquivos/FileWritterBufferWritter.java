1096df820194bf3390e5a41f23459d1c3b656090
Elisio Ricardo <elisio.silva@sysmap.com>
2023-10-11 14:17:09 -0300

    Adicionando novas classes de arquivos

--- src/main/java/Arquivos/FileWritterBufferWritter.java
+++ src/main/java/Arquivos/FileWritterBufferWritter.java
@@ -1,12 +1,24 @@
 package Arquivos;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWritterBufferWritter {
     public static void main(String[] args) {
 
-        String[] lines = new String[]{"good morning", "Good Afternoom", "Good night"};
+        String[] lines = new String[]{"Good morning", "Good Afternoom", "Good night"};
 
         String path = "C:\\Users\\elisio.silva\\Downloads\\JavaCompleto2023UdemyNelioAlves\\out.txt";
 
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) { //Se adicionar o true ele adiciona imformação, sem ele recria
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
